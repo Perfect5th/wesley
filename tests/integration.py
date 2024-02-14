19efbbd574a168db24a0c60e76a6e64bc64edf8a
Mitch Burton <mitch@mitchellburton.ca>
2024-02-14 11:30:38 -0800

    some stuff

--- tests/integration.py
+++ tests/integration.py
@@ -1,8 +1,31 @@
 """Integration tests for wesley."""
 
+import pathlib
+import subprocess
+import sys
+import tempfile
+
+SOURCE_DIR = pathlib.Path(__file__).parent.parent
+
 
 def test_init() -> None:
     """Executing `wesley init` results in a reasonable directory layout with which to
     start a static site.
+
+    We do this in a temporary directory for ease of cleanup.
     """
-    return
+    with tempfile.TemporaryDirectory() as tempdir:
+        try:
+            completed_process = subprocess.run(
+                [sys.executable, '-m', 'wesley', 'init'],
+                capture_output=True,
+                cwd=tempdir,
+                check=True,
+                text=True,
+                env={'PYTHONPATH': SOURCE_DIR},
+            )
+        except subprocess.CalledProcessError as cpe:
+            raise Exception(f"Error in init call: {cpe.stderr}")
+
+    assert len(completed_process.stdout)
+    assert '\N{CAT}\N{ZWJ}\N{BLACK LARGE SQUARE}' in completed_process.stdout
