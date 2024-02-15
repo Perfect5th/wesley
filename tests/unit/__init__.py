8022f9ecad663fcf492565e8e41bb5f00d5c2d89
Mitch Burton <mitch@mitchellburton.ca>
2024-02-14 21:40:15 -0800

    cleanup

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,4 +1,17 @@
 """Unit tests for wesley.
 
 Copyright © 2024 Mitch Burton
+
+This file is part of wesley.
+
+wesley is free software: you can redistribute it and/or modify it under the terms of the
+GNU General Public License as published by the Free Software Foundation, either version
+3 of the License, or (at your option) any later version.
+
+wesley is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with wesley. If
+not, see <https://www.gnu.org/licenses/>.
 """
